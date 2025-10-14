481acb13d7cee84b9266720f8fac92a58847fd61
Ratna Deep <ratna.ratna50@gmail.com>
2025-10-14 10:29:59 +0530

    Live 1.0.9
    Branding KS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hrbabu.tracking"
         minSdk = 23
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
